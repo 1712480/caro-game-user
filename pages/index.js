1915c8436d249a5308da5679b9e9670525291f28
Hung Nguyen <hungnguyen@Nguyens-MacBook-Pro.local>
2020-12-05 16:24:16 +0700

    removed package-lock

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Container } from 'reactstrap';
 
 export default function Home() {
+  const test = [];
+
+  useEffect(() => {
+    console.log(test);
+  }, [])
+
   return (
     <Container>Home page</Container>
   )
