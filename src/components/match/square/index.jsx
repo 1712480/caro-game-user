8cc333ead02999e483f2e443a44139d193342337
Linkin Ben <linkinben@localhost.localdomain>
2020-12-14 23:26:47 +0700

    Player Making Move In Room

--- src/components/match/square/index.jsx
+++ src/components/match/square/index.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './styles.module.scss';
 import { exeMove } from '../../../redux/currentMatch';
+import { selectUser } from '../../../redux/userSlice';
 
-function Square({ x, y, value, socket }) {
+function Square({ x, y, value, socket, roomId }) {
+  const [currentUser] = useState(useSelector(selectUser));
   const isEndGame = useSelector((state) => state.match.isEndGame);
   const dispatch = useDispatch();
   const clickBtn = () => {
     if (value === 0 && !isEndGame) {
-      socket.emit('client-make-move', { x, y });
+      // Do something with current player's turn here maybe
+      socket.emit('client-make-move', { player: currentUser.user.email, move: { x, y }, roomId });
       const action = exeMove({ x, y });
       dispatch(action);
     }
   };
+
+  socket.on(`server-resp-move${roomId}`, (response) => {
+    // Listen to opponent's move
+    if (response.player !== currentUser.user.email) {
+      const action = exeMove({ x: response.move.x, y: response.move.y });
+      dispatch(action);
+    }
+  });
 
   switch (value) {
   case 1:
--- src/pages/match/[index].jsx
+++ src/pages/match/[index].jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'reactstrap';
 import Board from '../../components/match/board';
 import styles from './styles.module.scss';
@@ -8,20 +8,25 @@ import EndGame from '../../components/match/endGame';
 import UserPlaying from '../../components/match/userPlaying';
 import Chat from '../../components/match/chat';
 import { restartGame } from '../../redux/currentMatch';
+import { selectUser } from '../../redux/userSlice';
 
 const Match = (props) => {
   const { socket } = props;
+  const [currentUser] = useState(useSelector(selectUser));
   const param = useRouter();
   socket.on('player-join-game', (response) => {
-    setTimeout(() => {
-      socket.emit('request-start-game', response);
-    }, 3000);
+    socket.emit('request-start-game', response);
   });
-  socket.on(`start-game-${param.query.index}`, () => {
-
-  });
-  socket.on('server-resp-move', () => {
-
+  socket.on(`start-game-${param.query.index}`, (response) => {
+    // Do something with turn when game start
+    // Checking if player is host and give turn here?
+    if (response.roomDetails.y.username !== currentUser.user.email) {
+      // Notify host's turn something (Modal and Stuff Would Be Nice)
+      // console.log('You are host and it\'s your turn!');
+    } else {
+      // Notify not challenger's turn
+      // console.log('Wait for host\'s first move!');
+    }
   });
 
   const dispatch = useDispatch();
@@ -35,7 +40,7 @@ const Match = (props) => {
         <UserPlaying name="Tran Nhut Kha" img="https://res.cloudinary.com/kh-ng/image/upload/v1607835120/caro/unnamed_rwk6xo.png" />
         <UserPlaying name="Waiting..." img="https://res.cloudinary.com/kh-ng/image/upload/v1607835120/caro/unnamed_rwk6xo.png" />
       </div>
-      <Board socket={socket} />
+      <Board socket={socket} roomId={param.query.index} />
       <Chat />
       <EndGame />
       <Button style={{ position: 'fixed', top: 100, left: 50 }} color="warning">Start</Button>
